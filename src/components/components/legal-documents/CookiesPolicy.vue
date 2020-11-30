0c5fadb12e37e19d5d00d6e3b964181a9dfdcbe1
tatsiana.zubel@icloud.com <tatsiana.zubel@icloud.com>
2020-11-30 17:49:00 +0200

    Added new components: CookiesPolicy, PrivacyPolicy, Recommendations, TermsAndConditions, LegalDocuments.

---
+++ src/components/components/legal-documents/CookiesPolicy.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="cookies-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-10 mx-auto">
+                    <div class="cookies-heading text-center">
+                        <h1>Cookies Policy</h1>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus lacinia odio et maximus.
+                        Donec fringilla id mauris sed eleifend. Sed convallis vehicula mi a tincidunt:
+                        <a href="#" target="_blank" class="url-link">link</a>.
+                    </p>
+                    <p>Quisque eu dolor in quam lobortis vestibulum. Donec risus eros, rhoncus in aliquam sit amet, varius
+                        a neque. Vestibulum sit amet fringilla urna, vel consectetur ex. Sed venenatis, nisl ac bibendum
+                        molestie, sem nisl elementum quam, eu ultricies enim tortor non diam. Etiam suscipit velit dignissim
+                        placerat malesuada.</p>
+                    <p>Phasellus lacinia suscipit tempus. Aliquam dignissim massa et enim faucibus, sit amet rutrum eros
+                        dictum. Aenean viverra luctus laoreet. Fusce lacinia enim vitae porttitor pellentesque.</p>
+                    <p>Integer tempus hendrerit felis ac imperdiet. Duis fermentum quam ut lectus varius vehicula.
+                        Donec condimentum augue et ultrices sodales. Integer libero lacus, sollicitudin sed nibh at,
+                        commodo facilisis augue. Nam laoreet enim metus, et blandit lorem venenatis eget. Suspendisse
+                        consequat arcu eget mi molestie, vel pulvinar lacus varius. Nam semper erat fermentum velit elementum
+                        euismod. Nam ut erat sed tellus accumsan placerat nec nec eros. Vivamus tempor ipsum auctor enim
+                        interdum, id congue enim rutrum. Vestibulum tempor nulla et ex tempor sodales. Sed porttitor massa
+                        magna, ac egestas lorem semper vel. Suspendisse ut iaculis odio, id vehicula orci. Suspendisse
+                        cursus diam vel mauris porttitor mattis. Fusce aliquet sit amet nibh ac ullamcorper:
+                        <a href="#" target="_blank" class="url-link">link</a>.
+                    </p>
+                    <p>Proin vitae sagittis lectus, id vulputate turpis. Mauris feugiat augue sit amet feugiat fringilla.
+                        Nullam semper nec sem id aliquet. Donec sollicitudin, nisi a malesuada auctor, urna ligula dictum
+                        est, id dictum nibh eros vel mi.</p>
+                    <p>Etiam non varius nisi. Fusce sollicitudin enim ultricies neque placerat vulputate. Aliquam interdum
+                        ipsum eget eros eleifend, eu fringilla ante mollis. Proin nec lorem nisl. Aenean nisl turpis,
+                        iaculis ac arcu et, varius accumsan nisl. Nam lectus diam, hendrerit sit amet finibus eget, feugiat
+                        vitae augue. Nam dignissim ante aliquam posuere auctor.</p>
+                    <table class="table table-bordered text-muted mt-4 mb-5">
+                        <thead>
+                        <tr>
+                            <th scope="col">TITLE</th>
+                        </tr>
+                        </thead>
+                        <tbody class="font-italic">
+                        <tr>
+                            <td>praesent elementum</td>
+                        </tr>
+                        <tr>
+                            <td>erat nec fringilla tincidunt</td>
+                        </tr>
+                        <tr>
+                            <td>odio tellus gravida sem</td>
+                        </tr>
+                        <tr>
+                            <td>itae malesuada ante ligula at sem</td>
+                        </tr>
+                        <tr>
+                            <td>aenean egestas</td>
+                        </tr>
+                        <tr>
+                            <td>lacus ac volutpat sagittis</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <p>Aenean rhoncus lacus eu tincidunt accumsan. Nunc auctor consequat ipsum, at porttitor orci pretium
+                        sit amet. Phasellus sit amet lectus felis. Fusce ut mi eget arcu fermentum maximus eget ac tortor.
+                        Etiam quam odio, auctor ut dignissim id, posuere ac ipsum.</p>
+                    <h4 class="mb-4">Curabitur in felis id ipsum vestibulum eleifend</h4>
+                    <p>Nam varius est est, quis eleifend augue tempor nec. Phasellus dictum mauris a justo ultricies, a
+                        porta libero vestibulum. Integer ac gravida sem, id eleifend orci. Ut ac congue dui, vitae porta
+                        est. Integer scelerisque viverra euismod. Nam varius sapien sit amet eros ullamcorper cursus.
+                        Phasellus scelerisque libero quis vulputate mattis. Proin non ipsum et enim mollis porta. Etiam at lectus turpis..</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .cookies-container {
+        padding-top: 115px;
+        padding-bottom: 50px;
+    }
+
+    .cookies-heading {
+        margin-bottom: 50px;
+    }
+
+    .cookies-heading::before {
+        content: "";
+        display: block;
+        width: 35px;
+        border-top: 2px solid #d83135;
+        margin: 0 auto 2rem;
+        text-align: center;
+    }
+
+    p {
+        margin-bottom: 30px;
+    }
+
+    h4 {
+        text-transform: uppercase;
+    }
+</style>
